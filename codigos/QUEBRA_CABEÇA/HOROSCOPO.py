740eed72cb3e6e77dc756fcb5430185bc7bfd3d4
Maciel Alves Pereira <130186739+macielalves@users.noreply.github.com>
2023-04-08 15:43:04 -0300

    Troca do código fonte por minha versão

--- codigos/QUEBRA_CABEÇA/HOROSCOPO.py
+++ codigos/QUEBRA_CABEÇA/HOROSCOPO.py
@@ -1,4 +1,7 @@
-# Thiago version
+from unicodedata import normalize
+import urllib.request, ssl
+
+
 def signo(dia, mes):
     if mes == 3:
         return 'Peixes' if dia < 21 else 'Áries'
@@ -17,40 +20,55 @@ def signo(dia, mes):
     if mes == 10:
         return 'Libra' if dia < 23 else 'Escorpião'
     if mes == 11:
-        return 'Escorpião' if dia < 22 else 'Sagitário'
+        return 'Escorpião' if dia < 22 else 'Sargitário'
     if mes == 12:
-        return 'Sagitário' if dia < 22 else 'Capricórnio'
+        return 'Sargitário' if dia < 22 else 'Capricórnio'
     if mes == 1:
         return 'Capricórnio' if dia < 20 else 'Aquário'
     if mes == 2:
         return 'Aquário' if dia < 19 else 'Peixes'
 
 
-def remover_acentos(texto):
-    from unicodedata import normalize
-    return normalize('NFKD', texto).encode('ASCII', 'ignore').decode('ASCII')
-
 def horoscopo(signo_desejado):
-    import urllib.request
-
     signo_formatado = remover_acentos(signo_desejado).lower()
     minha_url = 'https://www.horoscopovirtual.com.br/horoscopo/' + signo_formatado
 
     requisicao = urllib.request.Request(
         url=minha_url,
-        headers={'User-Agent': 'Mozila/5.0'}
+        headers={'User-Agent': 'Mozilla/5.0'}
     )
+    contexto_ssl = ssl.create_default_context()
+    contexto_ssl.check_hostname = False
+    contexto_ssl.verify_mode = ssl.CERT_NONE
 
-    resposta = urllib.request.urlopen(requisicao)
-
+    resposta = urllib.request.urlopen(requisicao, context=contexto_ssl)
     pagina = resposta.read().decode('utf-8')
-
-
     marcador_inicio = '<p class="text-pred">'
-    marcador_final = '</p>'
+    marcador_final = '<a class="webshare-link">Compartilhar</a>'
+
     inicio = pagina.find(marcador_inicio) + len(marcador_inicio)
     final = pagina.find(marcador_final, inicio)
-    return signo_desejado + ': ' + pagina[inicio:final]
+
+    return signo_desejado + ':' + pagina[inicio:final].strip()
+
+
+def main():
+    # Entrada de dados
+    nascimento = int(input('Digite sua data de nascimento no formato DDMMAAAA: '))
+
+    # Processamento
+    dia, mes, ano = separar_data(nascimento)
+    meu_signo = signo(dia, mes)
+    horoscopo_de_hoje = horoscopo(meu_signo)
+
+    # Saída de dados
+    print(horoscopo_de_hoje)
+
+
+def remover_acentos(texto):
+    texto = str(texto)
+    return normalize('NFKD', texto).encode('ASCII', 'ignore').decode('ASCII')
+
 
 def separar_data(dma):
     a = dma % 10000
@@ -59,15 +77,6 @@ def separar_data(dma):
     dma //= 100
     d = dma
     return d, m, a
-
-def main():
-
-    nascimento = int(input("Digite sua data de nascimento no formato DD/MMAAA: "))
-    dia, mes, _ = separar_data(nascimento)
-    meu_signo = signo(dia, mes)
-    horoscopo_de_hoje = horoscopo(meu_signo)
-
-    print(horoscopo_de_hoje)
 
 
 if __name__ == '__main__':
