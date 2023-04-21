56bb379bbc28e6b3f479a666c50ea7b1bfd97223
Maciel Alves <catce.2023111mtds0003@aluno.ifpi.edu.br>
2023-04-21 00:22:21 -0300

    Add comments

--- projetos/conversor_de_numeros/converte.py
+++ projetos/conversor_de_numeros/converte.py
@@ -1,2 +1,8 @@
-# Teste
-print('Hello World')
+# conversor de numeros
+
+def converte_numero(num):
+    # decimal 0 1 2 3 4 5 6 7 8 9
+    # octal 0 1 2 3 4 5 6 7
+    # hexadecimal 0 1 2 3 4 5 6 7 8 9 A B C D F
+    
+    
