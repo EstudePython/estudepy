35273e0a90345a80240fcdeeeccad47d6a6b2fa1
Maciel Alves <catce.2023111mtds0003@aluno.ifpi.edu.br>
2023-04-21 04:00:13 -0300

    debug de converte.py

--- projetos/conversor_de_numeros/converte.py
+++ projetos/conversor_de_numeros/converte.py
@@ -1,33 +1,56 @@
-# conversor de numeros
-
-def inverte_str(n):
-    # inverte aqui usando str[::-1]
-    return n[::-1]
-
+# conversor de sistemas numeros
 
 # a função recebe os parâmetros num para qualquer número e formato para o tipo.
-def converte_numero(num, formato='d'):
+def converte_numero(num, formato=2):
+    if num == '':
+        raise ValueError('Insira um valor que preste!')
 
     # decimal 0 1 2 3 4 5 6 7 8 9
     # octal 0 1 2 3 4 5 6 7
     # hexadecimal 0 1 2 3 4 5 6 7 8 9 A B C D F
     # binário 0 1
 
-    teste = 123
+    # comparam qual será o tipo numérico processado
+    if formato == 'd':
+        formato = 10
+    elif formato == 'b':
+        formato == 2
+    elif formato == 'o':
+        formato == 8
+    elif formato == 'h':
+        formato == 16
 
-    def converte_para_binario(num):
-        saida = ''
-        while 0 < num:
-            binario = num % 2
-            saida = str(saida) + str(binario)
-            tira_metade = num // 2
-            num = tira_metade
+    saida = ''
+    while 0 < num:
+        unidade = num % formato
+        saida = str(saida) + str(unidade)
+        tira_metade = num // formato
+        num = tira_metade
+        if num < 1:
+            numero = int(saida[::-1])
+            return numero, formato
 
-            if num < 1:
-                inverte_str(saida)
-                return int(saida)
-
-    converte_para_binario(teste)
+    return num, formato
 
 
-converte_numero(10)
+# x, y = int(input('Número: ')), int(input('Tipo: '))
+# Debug
+x, y = 396, 16
+
+x, y = converte_numero(x, y)
+
+u = c = ''
+if y == 2:
+    u = 'binário'
+    c = str(x).zfill(8)
+elif y == 8:
+    u = 'octal'
+elif y == 10:
+    u = 'decimal'
+elif y == 16:
+    u = 'hexadecimal'
+else:
+    u = 'Sei lá'
+
+
+print(f'{c if y == 2 else x} {u}')
