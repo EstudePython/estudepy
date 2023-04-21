c6f6e8f9f06cc51eb70f975604c2bc4c2116c9e2
Maciel Alves <catce.2023111mtds0003@aluno.ifpi.edu.br>
2023-04-21 17:40:10 -0300

    upgrade converter and add settings.json

--- projetos/conversor_de_numeros/converte.py
+++ projetos/conversor_de_numeros/converte.py
@@ -1,7 +1,8 @@
+
 # conversor de sistemas numeros
 
 # a função recebe os parâmetros num para qualquer número e formato para o tipo.
-def converte_numero(num, formato=2):
+def converte_numero(num, formato='b'):
     if num == '':
         raise ValueError('Insira um valor que preste!')
 
@@ -11,58 +12,60 @@ def converte_numero(num, formato=2):
     # binário 0 1
 
     # comparam qual será o tipo numérico processado
-    if formato == 'd':
-        formato = 10
-    elif formato == 'b':
-        formato = 2
-    elif formato == 'o':
-        formato = 8
+    formato = formato.lower()
+    valor_de_tipo = 0
+    if formato in ('decimal', 'dec', 'd'):
+        valor_de_tipo = 10
+    elif formato in ('binario', 'bin', 'b'):
+        valor_de_tipo = 2
+    elif formato in ('octal', 'oct', 'o'):
+        valor_de_tipo = 8
     # contem erros ao processar
-    elif formato == 'h':
-        formato = 16
+    elif formato in ('hexadecimal', 'hex', 'h') or formato[:4] in 'hex':
+        valor_de_tipo = 16
 
     # essa lógica funciona até octa
-    saida = ''
+    valor_invertido = ''
     while 0 < num:
-        unidade = num % formato
-        saida = saida + str(unidade)
-        tira_metade = num // formato
+        unidade = num % valor_de_tipo if valor_de_tipo != 0 else 0
+        valor_invertido = valor_invertido + str(unidade)
+        tira_metade = num // valor_de_tipo if valor_de_tipo != 0 else 0
         num = tira_metade
         if num < 1:
-            numero = int(saida[::-1])
-            return numero, formato
-
-    return num, formato
-
-
-def Hex_to_dec():
-    pass
-
-
-def formata_numero(x, y):
-    # Debug
-    x, y = converte_numero(x, y)
-
-    u = k = ''
-    if y == 2:
-        u = 'binário'
-        k = str(x).zfill(8)
-    elif y == 8:
-        u = 'octal'
-    elif y == 10:
-        u = 'decimal'
-    elif y == 16:
-        u = 'hexadecimal'
+            saida = valor_invertido[::-1]
+            if valor_de_tipo == 2:
+                return '0b' + saida, 'binário'
+            elif valor_de_tipo == 8:
+                return '0o' + saida, 'octal'
+            elif valor_de_tipo == 10:
+                return '0d' + saida, 'decimal'
+            elif valor_de_tipo == 16:
+                return '0h' + saida, 'hexadecimal'
+            else:
+                return saida, formato
     else:
-        u = 'Sei lá'
-    return x, y, k, u
+        return num, formato
+
+
+def formata_hexadecimal(numero):
+    if ('A' or 'B' or 'C' or 'D' or 'F') in str(numero).upper():
+        # é hexadecimal
+        pass
 
 
 def main():
-    # x, y = int(input('Número: ')), int(input('Tipo: '))
-    x, y, k, u = formata_numero(x=1024, y=2)
-    saida_numero = k if y == 2 else x
-    print(f'{saida_numero}')
+
+    # Entrada
+    x_in, y_in = int(input('Número: ')), str(input('Tipo: ')).strip()
+
+    # Processamento
+    x_out, y_out = converte_numero(x_in, y_in)
+    saida_numero = x_out
+
+    # Saída
+    print(f'{saida_numero}({y_out}) = {hex(x_in)} = {len(saida_numero) - 3}n')
+
+    # Armazenamento
 
 
 if __name__ == '__main__':
