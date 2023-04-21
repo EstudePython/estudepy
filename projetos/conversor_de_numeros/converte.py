17aa9f58464bdd58d9b377abf3a437fcb931087e
Maciel Alves <catce.2023111mtds0003@aluno.ifpi.edu.br>
2023-04-21 01:30:52 -0300

    update converte.py and rename busca_youtube.py to busca_url.py

--- projetos/conversor_de_numeros/converte.py
+++ projetos/conversor_de_numeros/converte.py
@@ -1,8 +1,9 @@
 # conversor de numeros
 
-def converte_numero(num):
+def converte_numero(num, formato='d'):
     # decimal 0 1 2 3 4 5 6 7 8 9
     # octal 0 1 2 3 4 5 6 7
     # hexadecimal 0 1 2 3 4 5 6 7 8 9 A B C D F
+    # binário 0 1
     
-    
+    if num 
--- url_lib/busca_url.py
+++ url_lib/busca_url.py
@@ -1,16 +1,16 @@
-def busca_youtube(link):
+def busca_url(link):
     import urllib.request
     import ssl
 
-    requisicao = urllib.request.Request(
-        url=link,
-        headers={'User-Agent': 'Mozilla/5.0'}
-    )
     contexto_ssl = ssl.create_default_context()
     contexto_ssl.check_hostname = False
     contexto_ssl.verify_mode = ssl.CERT_NONE
 
-    resposta = urllib.request.urlopen(requisicao, context=contexto_ssl)
+    resposta = urllib.request.urlopen(urllib.request.Request(
+        url=link,
+        headers={'User-Agent': 'Mozilla/5.0'}
+    ), context=contexto_ssl)
+
     pagina = resposta.read().decode('utf-8')
     marcador_inicio = '<body>'
     marcador_final = '</body>'
@@ -21,5 +21,4 @@ def busca_youtube(link):
     return pagina[inicio:final].strip()
 
 
-print(busca_youtube(
-    'https://www.youtube.com/watch?v=hdDHg1p3YVc'))
+print(busca_url('https://www.google.com'))
